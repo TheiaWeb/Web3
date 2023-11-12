6b02e96968b4117267d44cd118b6ca148ce3a49d
Pauline Mila Alonso <113476814+Milhaal@users.noreply.github.com>
2023-11-12 19:05:35 +0100

    dernières modifications front

--- production/js/script.js
+++ production/js/script.js
@@ -4,12 +4,15 @@ function toggleMenu() {
     const menuIcon = document.getElementById('menuIcon');
     const burgerIconSrc = 'img/navbar-burger.png';
     const closeIconSrc = 'img/navbar-close.png';
+    const body = document.body;
   
     if (menu.classList.contains('menu-open')) {
       menu.classList.remove('menu-open');
+      body.classList.remove('no-scroll');
       menuIcon.src = burgerIconSrc;
     } else {
       menu.classList.add('menu-open');
+      body.classList.add('no-scroll');
       menuIcon.src = closeIconSrc;
     }
   }
