dcce107a08b7330cc5f384ae17fc3946fb513a67
Haxxxxxx <62444678+Haxxxxxx@users.noreply.github.com>
2023-11-13 17:54:05 +0100

    function laptop

--- functions/index.js
+++ functions/index.js
@@ -17,3 +17,45 @@ const logger = require("firebase-functions/logger");
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const nodemailer = require('nodemailer');
+
+admin.initializeApp();
+
+// Configure Nodemailer with your email service credentials
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        type: 'OAuth2',
+        user: functions.config().emailservice.user,
+        clientId: '891972920996-r9jjg81ou5f3bhkh5h69rrhl54q73ln2.apps.googleusercontent.com',
+        clientSecret: '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',
+        refreshToken: 'VOTRE_REFRESH_TOKEN',
+        accessToken: 'VOTRE_ACCESS_TOKEN', // Optionnel, généré automatiquement
+    },
+});
+exports.sendEmailConfirmation = functions.firestore
+  .document('users/{userId}')
+  .onCreate((snap, context) => {
+    const newData = snap.data();
+    const email = newData.email; // Assuming the saved data contains an 'email' field
+
+    const mailOptions = {
+      from: '3space.contact@gmail.com',
+      to: email,
+      subject: 'Confirmation Email',
+      text: `Hello, your data has been saved successfully!` // Customize your email message
+    };
+
+    return transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error('Error sending email:', error);
+        return null;
+      }
+      console.log('Email sent:', info.response);
+      return null;
+    });
+  });
