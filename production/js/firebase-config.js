fb378b8e94d5747188bddcad007be97dcf5d1eed
Haxxxxxx <62444678+Haxxxxxx@users.noreply.github.com>
2023-11-13 12:17:43 +0100

    Separation Config + functions

--- production/js/firebase-config.js
+++ production/js/firebase-config.js
@@ -1,7 +1,9 @@
 
   // Import the functions you need from the SDKs you need
-//   import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
-//   import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-analytics.js";
+   import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
+   import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-analytics.js";
+   import { getFirestore } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-firestore.js";
+
 //   import { getFirestore } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-firestore.js"
    // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
@@ -10,8 +12,8 @@
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 // Import the functions you need from the SDKs you need
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -30,4 +32,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-const db = firebase.firestore();
+const db = getFirestore(app);
+export { db };
--- production/js/script.js
+++ production/js/script.js
@@ -18,3 +18,35 @@ function toggleMenu() {
   }
   //#endregion
 
+  // Your main JavaScript file
+import { addUserToNewsletter } from './firebase.js';
+
+document.getElementById('newsletterForm').addEventListener('submit', function (event) {
+  event.preventDefault();
+  const email = document.getElementById('emailInput').value;
+  const firstWord = email.split('@')[0].toLowerCase();
+  const now = new Date();
+  const timestamp = now.toString();
+
+  addUserToNewsletter(email, firstWord, timestamp)
+    .then(() => {
+      console.log('Document written with ID');
+
+      // UI Success Handling
+      document.getElementById('newsletter-send').style.display = 'block';
+      setTimeout(() => {
+        document.getElementById('newsletter-send').classList.add('hide');
+        setTimeout(() => {
+          document.getElementById('newsletter-send').style.display = 'none';
+          document.getElementById('newsletter-send').classList.remove('hide');
+        }, 500); // Assuming your CSS transition duration is 0.3 seconds
+      }, 4000);
+    })
+    .catch((error) => {
+      console.error('Error adding document: ', error);
+      // Handle error or UI updates here
+    });
+
+  document.getElementById("newsletterForm").reset();
+  // Additional UI logic if needed
+});
