7697932f547ba957b1b8337801cffe8cc3a23f05
Haxxxxxx <vinc.charles0@gmail.com>
2023-11-24 13:09:14 +0100

    Update nav bar mobile

--- production/js/script.js
+++ production/js/script.js
@@ -1,21 +1,21 @@
 //#region TOGGLE MENU
 function toggleMenu() {
-    const menu = document.getElementById('mobileMenu');
-    const menuIcon = document.getElementById('menuIcon');
-    const burgerIconSrc = 'img/navbar-burger.png';
-    const closeIconSrc = 'img/navbar-close.png';
-    const body = document.body;
-  
-    if (menu.classList.contains('menu-open')) {
+  const menu = document.getElementById('mobileMenu');
+  const menuIcon = document.getElementById('menuIcon');
+  const burgerIconSrc = 'img/navbar-burger.png';
+  const closeIconSrc = 'img/navbar-close.png';
+  const body = document.body;
+
+  if (menu.classList.contains('menu-open')) {
       menu.classList.remove('menu-open');
       body.classList.remove('no-scroll');
       menuIcon.src = burgerIconSrc;
-    } else {
+  } else {
       menu.classList.add('menu-open');
       body.classList.add('no-scroll');
       menuIcon.src = closeIconSrc;
-    }
   }
+}
   //#endregion
 
   //#region TOGGLE MENU
